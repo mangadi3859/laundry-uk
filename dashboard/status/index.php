15efbf4d6e6520a5f54d42e839a9755255fdc41e
mangadi3859 <wkomangadi44@gmail.com>
2024-04-18 09:10:06 +0800

    status index

--- dashboard/status/index.php
+++ dashboard/status/index.php
@@ -35,6 +35,7 @@ if (!empty($member)) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -52,13 +53,14 @@ if (!empty($member)) {
     <script src="../../public/js/global.js" defer></script>
     <!-- <script src="../../public/js/transaksi-view.js" defer></script> -->
 </head>
+
 <body>
     <?php include "../../components/sidebar.php" ?>
     <div class="main-container">
         <img class="banner" src="../../public/assets/status-banner.jpg">
         <main id="main">
             <?php include "../../components/navbar.php" ?>
-            
+
             <div class="action-table">
                 <form action="" method="GET" class="action-form">
                     <input name="token" type="text" placeholder="Cari token member" class="input-action">
@@ -84,97 +86,103 @@ if (!empty($member)) {
 
             <div class="table-container" <?= empty($transaksi) ? "hidden" : "" ?>>
                 <table>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Invoice</th>
-                                <th>Tanggal Transaksi</th>
-                                <th>Batas Waktu</th>
-                                <th>Tanggal Bayar</th>
-                                <th>Status</th>
-                                <th>Pembayaran</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            foreach ($transaksi as $k => $row) {
-                                $warning = array_pop($row);
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Invoice</th>
+                            <th>Tanggal Transaksi</th>
+                            <th>Batas Waktu</th>
+                            <th>Tanggal Bayar</th>
+                            <th>Status</th>
+                            <th>Pembayaran</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($transaksi as $i => $row) {
+                            $warning = array_pop($row);
 
 
-                                $row["tgl"] = date("Y/m/d", strtotime($row["tgl"]));
-                                $row["tgl_bayar"] = date("Y/m/d", strtotime($row["tgl_bayar"]));
-                                $row["batas_waktu"] = date("Y/m/d", strtotime($row["batas_waktu"]));
-                                echo "<tr>";
-                                foreach ($row as $k => $data) {
-                                    if ($k == "invoice" && $warning) {
-                                        echo "<td><a data-warning='{$row['batas_waktu']}' title='Batas waktu terlewat' class='warning fa-triangle-exclamation fas'></a> $data</td>";
-                                        continue;
-                                    }
-
-                                    if ($k == "batas_waktu" && $warning) {
-                                        echo "<td><a data-warning='{$row['batas_waktu']}' title='Batas waktu terlewat' class='warning fa-triangle-exclamation fas'></a> $data</td>";
-                                        continue;
-                                    }
-
-                                    if ($k == "tgl_bayar" && $row["dibayar"] != "dibayar") {
-                                        echo "<td>-</td>";
-                                        continue;
-                                    }
-
-                                    if ($k == "status") {
-                                        $bg = "";
-                                        $text = "";
-                                        switch ($data) {
-                                            case "baru": {
-                                                    $bg = "#cce5ff";
-                                                    $text = "#004085";
-                                                    break;
-                                                }
-                                            case "proses": {
-                                                    $bg = "#fff3cd";
-                                                    $text = "#856404";
-                                                    break;
-                                                }
-                                            case "selesai": {
-                                                    $bg = "#d4edda";
-                                                    $text = "#155724";
-                                                    break;
-                                                }
-                                            default: {
-                                                    $bg = "#d6d8d9";
-                                                    $text = "#1b1e21";
-                                                    break;
-                                                }
-                                        }
-
-                                        echo "<td><div class='td-info'><span style='color: $text; background-color: $bg; padding: .25rem .5rem; border-radius: .25rem; border: 1px solid $text;'>$data</span></div></td>";
-                                    } else if ($k == "dibayar") {
-                                        $bg = "";
-                                        $text = "";
-                                        switch ($data) {
-                                            case "belum_dibayar": {
-                                                    $bg = "#fff3cd";
-                                                    $text = "#856404";
-                                                    break;
-                                                }
-                                            default: {
-                                                    $bg = "#d4edda";
-                                                    $text = "#155724";
-                                                    break;
-                                                }
-                                        }
-
-                                        echo "<td><div class='td-info'><span style='color: $text; background-color: $bg; padding: .25rem .5rem; border-radius: .25rem; border: 1px solid $text;'>$data</span></div></td>";
-                                    } else
-                                        echo "<td>$data</td>";
+                            $row["tgl"] = date("Y/m/d", strtotime($row["tgl"]));
+                            $row["tgl_bayar"] = date("Y/m/d", strtotime($row["tgl_bayar"]));
+                            $row["batas_waktu"] = date("Y/m/d", strtotime($row["batas_waktu"]));
+                            $index = $i + 1;
+                            echo "<tr>";
+                            foreach ($row as $k => $data) {
+                                if ($k == "id") {
+                                    echo "<td>$index</td>";
+                                    continue;
                                 }
-                                echo "</tr>";
+                                if ($k == "invoice" && $warning) {
+                                    echo "<td><a data-warning='{$row['batas_waktu']}' title='Batas waktu terlewat' class='warning fa-triangle-exclamation fas'></a> $data</td>";
+                                    continue;
+                                }
+
+                                if ($k == "batas_waktu" && $warning) {
+                                    echo "<td><a data-warning='{$row['batas_waktu']}' title='Batas waktu terlewat' class='warning fa-triangle-exclamation fas'></a> $data</td>";
+                                    continue;
+                                }
+
+                                if ($k == "tgl_bayar" && $row["dibayar"] != "dibayar") {
+                                    echo "<td>-</td>";
+                                    continue;
+                                }
+
+                                if ($k == "status") {
+                                    $bg = "";
+                                    $text = "";
+                                    switch ($data) {
+                                        case "baru": {
+                                                $bg = "#cce5ff";
+                                                $text = "#004085";
+                                                break;
+                                            }
+                                        case "proses": {
+                                                $bg = "#fff3cd";
+                                                $text = "#856404";
+                                                break;
+                                            }
+                                        case "selesai": {
+                                                $bg = "#d4edda";
+                                                $text = "#155724";
+                                                break;
+                                            }
+                                        default: {
+                                                $bg = "#d6d8d9";
+                                                $text = "#1b1e21";
+                                                break;
+                                            }
+                                    }
+
+                                    echo "<td><div class='td-info'><span style='color: $text; background-color: $bg; padding: .25rem .5rem; border-radius: .25rem; border: 1px solid $text;'>$data</span></div></td>";
+                                } else if ($k == "dibayar") {
+                                    $bg = "";
+                                    $text = "";
+                                    switch ($data) {
+                                        case "belum_dibayar": {
+                                                $bg = "#fff3cd";
+                                                $text = "#856404";
+                                                break;
+                                            }
+                                        default: {
+                                                $bg = "#d4edda";
+                                                $text = "#155724";
+                                                break;
+                                            }
+                                    }
+
+                                    echo "<td><div class='td-info'><span style='color: $text; background-color: $bg; padding: .25rem .5rem; border-radius: .25rem; border: 1px solid $text;'>$data</span></div></td>";
+                                } else
+                                    echo "<td>$data</td>";
                             }
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </main>
     </div>
 </body>
+
 </html>
